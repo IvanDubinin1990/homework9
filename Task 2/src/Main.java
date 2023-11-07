301788dd8c35e4a27f10c7db28b9464c2aaaba1a
Кристина <dubininivan1990@gmail.com>
2023-11-07 19:46:22 +0400

    Выполнено задание из комментария

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static int x;
+
     public static void main(String[] args) {
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+
+        x = inputInt();
+        System.out.println(checkInputInt(x));
+    }
+
+    public static int inputInt() {
+        System.out.println("Введите число: ");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        return x;
+    }
+
+    public static boolean checkInputInt (int y) {
+        if (y % 2 == 0) {
+            return true;
+        }else{
+            return false;
+        }
     }
 }
